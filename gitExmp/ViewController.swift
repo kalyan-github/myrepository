2f2002dbe88898f26c9382bd75ee154a3e2bbad5
Vajrala Kalyan Naga Kumar <kalyankumar.191294@achiever-42.local>
2019-07-24 13:10:02 +0530

    Initial Commit

---
+++ gitExmp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitExmp
+//
+//  Created by Vajrala Kalyan Naga Kumar on 7/24/19.
+//  Copyright © 2019 Vajrala Kalyan Naga Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
